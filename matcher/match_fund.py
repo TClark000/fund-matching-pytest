ff05e98c0a080fe748188e63a52e9c92513f4a01
teresa <teresajclark@gmail.com>
2021-02-21 17:38:36 +0000

    match_fund objects valid

--- matcher/match_fund.py
+++ matcher/match_fund.py
@@ -10,3 +10,8 @@ class MatchFund(object):
             self.total_amount = float(total_amount)
         except TypeError as e:
             raise BadRequestException("Total amount cannot be expressed as a float, with error %s" % str(e))
+
+        self.match_order = match_order
+        self.matching_ratio = match_ratio
+
+        self.matching_ratio_as_float_multiplier = self.matching_ratio[0] / self.matching_ratio[1]
--- tests/test_match_fund.py
+++ tests/test_match_fund.py
@@ -1,6 +1,7 @@
 from matcher.match_fund import MatchFund
 import random
 import uuid
+import pytest
 
 def test_match_fund_is_valid():
     """
@@ -8,7 +9,9 @@ def test_match_fund_is_valid():
     total_amount is float
     match_order is int
     matching_ratio is list
-
+    matching_ratio has a len of 2
+    matching_ratio lh rh are persistent
+    matching_ratio defaults 1:1
     """
     match_fund_id = uuid.uuid4().hex
     total_amount = 100000.5
@@ -20,3 +23,23 @@ def test_match_fund_is_valid():
     assert isinstance(match_fund.total_amount, float)
     assert isinstance(match_fund.match_order, int)
     assert isinstance(match_fund.matching_ratio, list)
+
+    match_ratio = match_fund.matching_ratio
+
+    assert len(match_ratio) == 2
+    lhs, rhs = match_ratio
+    assert lhs == 2
+    assert rhs == 1
+
+    another_match_fund = MatchFund(match_fund_id, total_amount, match_order)
+    lhs, rhs = another_match_fund.matching_ratio
+    assert lhs == 1
+    assert rhs == 1
+
+def test_match_fund_total_amount_other():
+    match_fund_id = uuid.uuid4().hex
+    total_amount = "500g"
+    match_order = random.randint(0, 10)
+
+    with pytest.raises(ValueError):
+        MatchFund(match_fund_id, total_amount, match_order)
